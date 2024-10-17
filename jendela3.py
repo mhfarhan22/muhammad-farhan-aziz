81a48abd943c5f59db41865937b77833ebca6044
mhfarhan22 <mfarhanaziz02@gmail.com>
2024-10-17 20:21:37 +0700

    Add files via upload

---
+++ jendela3.py
@@ -0,0 +1,15 @@
+import PySimpleGUI as sg
+sg.theme("DarkGreen4")
+sg.theme_text_color("#FFFF00")
+window = sg.Window(title="Profile",
+                  layout=[[sg.Text("SELAMAT DATANG DI KELAS",
+                                   font=("Arial","25,italic","bold","underline"))],
+                          [sg.Text("NPM  : 2210010317 " )],        
+                          [sg.Text("Nama  : Farhan AZIZ ")],
+                          [sg.Text("Kelas : 5 M Regular Banjarmasin ")]
+],
+                  size=(400,200),
+                  font=("Times", 18))
+
+window()
+window.close()
